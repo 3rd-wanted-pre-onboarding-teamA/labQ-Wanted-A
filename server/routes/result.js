62885a4e288d02f03e0e386bdf1ea6c9458f2807
syoungee <sunyoungah69@gmail.com>
2022-06-30 22:33:08 +0900

    Feat: add new API return json

--- server/routes/result.js
+++ server/routes/result.js
@@ -1,19 +1,27 @@
 const express = require("express");
-const drainpipeInfo = require('../../request/drainPipe');
-const rainFallInfo = require('../../request/rainFall');
+const drainpipeInfo = require("../../request/drainPipe");
+const rainFallInfo = require("../../request/rainFall");
 
 const router = express.Router();
 
 router.get("/:code", async (req, res) => {
   const code = req.params.code;
   const drainPipeArr = await drainpipeInfo(code);
-  const area = drainPipeArr[0].location;
+  const area = drainPipeArr.row[0].GUBN_NAM;
   const rainFallArr = await rainFallInfo(area);
-  console.log('----하수관로 수위 현황----');
-  console.log(drainPipeArr);
-  console.log();
-  console.log('----강우량 현황----');
-  console.log(rainFallArr);
+
+  if (drainPipeArr.RESULT.MESSAGE === "정상 처리되었습니다") {
+    let res = {
+      CODE: 200,
+      GUBN: area,
+      GUBN_NUM: code,
+      DRAINPIPE: drainPipeArr.row,
+      RAINFALL: rainFallArr.row,
+    };
+    console.log(JSON.stringify(res, undefined, 2));
+    return JSON.stringify(res);
+  }
+  // TODO: ERROR시 CODE_NUM 띄워주기
 });
 
 module.exports = router;
