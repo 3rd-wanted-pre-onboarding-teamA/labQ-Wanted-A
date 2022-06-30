1dc79497c56037eb3096bac201ee289736d8b5cc
syoungee <sunyoungah69@gmail.com>
2022-06-30 22:28:33 +0900

    Feat: add time, code data in func drainpipeInfo

--- request/drainPipe.js
+++ request/drainPipe.js
@@ -1,24 +1,48 @@
-// "http://openAPI.seoul.go.kr:8088/(인증키)/json/DrainpipeMonitoringInfo/1/5/01/2019120614/2019120615";
 const dotenv = require("dotenv");
 const request = require("request");
 
 dotenv.config();
 
+Date.prototype.YYYYMMDDHH = function () {
+  var yyyy = this.getFullYear().toString();
+  var MM = pad(this.getMonth() + 1, 2);
+  var dd = pad(this.getDate(), 2);
+  var hh = pad(this.getHours(), 2);
+
+  return yyyy + MM + dd + hh;
+};
+
+Date.prototype.YYYYMMDDHH_1hour = function () {
+  var yyyy = this.getFullYear().toString();
+  var MM = pad(this.getMonth() + 1, 2);
+  var dd = pad(this.getDate() - 1, 2);
+  var hh = pad(this.getHours() - 1, 2);
+
+  return yyyy + MM + dd + hh;
+};
+
+function pad(number, length) {
+  var str = "" + number;
+  while (str.length < length) {
+    str = "0" + str;
+  }
+  return str;
+}
+
 const drainpipeInfo = (code) => {
   const url = "http://openAPI.seoul.go.kr:8088/";
   const SERVICE_KEY = process.env.WATER_LEVEL_API_KEY;
-  const requestUrl = `${url}${SERVICE_KEY}/json/DrainpipeMonitoringInfo/1/5/${code}/2022062914/2022062915`;
-  const data = [];
+  let nowDate = new Date();
+  let nowDateString = nowDate.YYYYMMDDHH().toString();
+  let oneHourBeforeDateString = nowDate.YYYYMMDDHH_1hour().toString();
+  const requestUrl = `${url}${SERVICE_KEY}/json/DrainpipeMonitoringInfo/1/5/${code}/${oneHourBeforeDateString}/${nowDateString}`;
   return new Promise((resolve, reject) => {
     request(requestUrl, (error, response, body) => {
-      JSON.parse(body).DrainpipeMonitoringInfo.row.forEach((elem) => {
-        data.push({
-          location: elem.GUBN_NAM,
-          waterLevel: elem.MEA_WAL,
-          date: elem.MEA_YMD,
-        });
-      });
-      resolve(data);
+      if (!error && response.statusCode == 200) {
+        resolve(JSON.parse(body).DrainpipeMonitoringInfo);
+      } else {
+        reject(error);
+      }
     });
   });
 };
