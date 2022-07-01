32481f1cdfd4768c5f2995a5ca5995844a7828c5
hjy <heojungyeon.97@gmail.com>
2022-07-01 13:50:55 +0900

    Refactor: 날짜 전처리 방식 변경, 소수점 자르기, 칼럼 순서 변경

--- server/routes/result2.js
+++ server/routes/result2.js
@@ -4,6 +4,18 @@ const rainFallInfo = require("../../request/rainFall");
 
 const router = express.Router();
 
+/**
+ * @code writer 황선영
+ * @description 구분 코드로 가져온 두 가지 api를 병렬 처리한 API 구현
+ * @ref 서울시 하수관로 수위 현황 openAPI: "https://data.seoul.go.kr/dataList/OA-2527/S/1/datasetView.do"
+ * @ref 서울시 강우량 정보 openAPI: "http://data.seoul.go.kr/dataList/OA-1168/S/1/datasetView.do"
+ *
+ * @GET("/api/result2?code=06")
+ * @param GUBN
+ *
+ * @returns json
+ */
+
 router.get("/", async (req, res) => {
   const code = req.query.code;
   const drainPipeArr = await drainpipeInfo(code);
@@ -12,11 +24,11 @@ router.get("/", async (req, res) => {
 
   if (drainPipeArr.RESULT.MESSAGE === "정상 처리되었습니다") {
     let res = {
-      CODE: 200,
-      GUBN: area,
-      GUBN_NUM: code,
-      DRAINPIPE: drainPipeArr.row,
-      RAINFALL: rainFallArr.row,
+      CODE: 200, //처리상태
+      GUBN: area, // 구이름
+      GUBN_NUM: code, // 구코드
+      DRAINPIPE: drainPipeArr.row.slice(-10, -1), // 1000개 중 뒤에서 10개만 가져오기
+      RAINFALL: rainFallArr.row.slice(-10, -1), // 1시간 정도의 데이터 가져오기
     };
     console.log(JSON.stringify(res, undefined, 2));
     return JSON.stringify(res);
