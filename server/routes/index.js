46933f3c529e176f18eddb311a96c47a30495ecb
syoungee <sunyoungah69@gmail.com>
2022-07-01 10:37:43 +0900

    Refactor: remove files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const waterlevel = require("./waterLevel");
-const rainFall = require("./rainFall");
-const result = require('./result');
+const result = require("./result");
 const router = express.Router();
 
-router.use("/WaterLevel", waterlevel);
-router.use("/RainFall", rainFall);
-router.use('/result', result);
+router.use("/result", result);
 
 module.exports = router;
