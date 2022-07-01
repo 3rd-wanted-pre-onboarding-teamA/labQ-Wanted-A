73a2189cdb7e31119c295b7a9926ca6e629a68d4
syoungee <sunyoungah69@gmail.com>
2022-07-01 11:54:21 +0900

    Refactor: remove funcs

--- request/drainPipe.js
+++ request/drainPipe.js
@@ -4,32 +4,6 @@ require("date-utils");
 
 dotenv.config();
 
-Date.prototype.YYYYMMDDHH = function () {
-  var yyyy = this.getFullYear().toString();
-  var MM = pad(this.getMonth() + 1, 2);
-  var dd = pad(this.getDate(), 2);
-  var hh = pad(this.getHours(), 2);
-
-  return yyyy + MM + dd + hh;
-};
-
-Date.prototype.YYYYMMDDHH_1hour = function () {
-  var yyyy = this.getFullYear().toString();
-  var MM = pad(this.getMonth() + 1, 2);
-  var dd = pad(this.getDate(), 2);
-  var hh = pad(this.getHours() - 1, 2);
-
-  return yyyy + MM + dd + hh;
-};
-
-function pad(number, length) {
-  var str = "" + number;
-  while (str.length < length) {
-    str = "0" + str;
-  }
-  return str;
-}
-
 const drainpipeInfo = (code) => {
   const url = "http://openAPI.seoul.go.kr:8088/";
   const SERVICE_KEY = process.env.WATER_LEVEL_API_KEY;
