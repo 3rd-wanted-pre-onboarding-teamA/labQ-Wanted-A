f01f4f5a96c7259e0dd4b373eee15bf378534127
syoungee <sunyoungah69@gmail.com>
2022-07-27 11:25:20 +0900

    refactor: 주석 정리 및 prettier 적용

--- request/rainFall.js
+++ request/rainFall.js
@@ -1,13 +1,12 @@
 const dotenv = require("dotenv");
 const request = require("request");
 
+dotenv.config();
 
-dotenv.config(); 
-
-// 서울시 하수관로 수위 현황 
+// 서울시 하수관로 수위 현황
 // open API: https://data.seoul.go.kr/dataList/OA-2527/S/1/datasetView.do
 const rainFall = (area) => {
-  const url = "http://openapi.seoul.go.kr:8088/"; 
+  const url = "http://openapi.seoul.go.kr:8088/";
   const SERVICE_KEY = process.env.RAIN_FALL_API_KEY;
   const localName = area + "구";
   const num = 1000;
@@ -16,9 +15,10 @@ const rainFall = (area) => {
   return new Promise((resolve, reject) => {
     request(requestUrl, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        resolve(JSON.parse(body).ListRainfallService); 
+        resolve(JSON.parse(body).ListRainfallService);
         // fulfilled - 상태코드가 200일 때 JSON에 있는 데이터 출력
-      } else { // rejected - 에러 났을 때 에러 출력
+      } else {
+        // rejected - 에러 났을 때 에러 출력
         console.log(error);
         reject(error);
       }
